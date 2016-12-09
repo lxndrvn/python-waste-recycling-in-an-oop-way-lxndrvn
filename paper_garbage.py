899997d47f2aac13f8756a178b78b73f069a1654
lxndrvn <i1990522@gmail.com>
2016-12-09 20:59:15 +0100

    Add code for PaperGarbage

--- paper_garbage.py
+++ paper_garbage.py
@@ -2,4 +2,9 @@ from garbage import Garbage
 
 
 class PaperGarbage(Garbage):
-    pass
+    def __init__(self, name, is_squeezed):
+        super(PaperGarbage, self).__init__(name) # kerdes
+        self.is_squeezed = is_squeezed
+        
+    def squeeze(self):
+        self.is_squeezed = True
