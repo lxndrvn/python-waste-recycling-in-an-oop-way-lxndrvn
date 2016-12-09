d493c056bde66170dcec9a6766180a7dad64386e
lxndrvn <i1990522@gmail.com>
2016-12-09 21:00:28 +0100

    Add dustbin code

--- dustbin.py
+++ dustbin.py
@@ -5,4 +5,34 @@ from dustbin_content_error import DustbinContentError
 
 
 class Dustbin:
-    pass
+    def __init__(self, color):
+        self.color = color
+        self.paper_content = []
+        self.plastic_content = []
+        self.house_waste_content = []
+    
+    def throw_out_garbage(self, garbage):
+        
+        if isinstance(garbage, PlasticGarbage):
+            if garbage.is_clean:
+                self.plastic_content.append(garbage)
+            else:
+                raise DustbinContentError
+            
+        elif isinstance(garbage, PaperGarbage):
+            if garbage.is_squeezed:
+                self.paper_content.append(garbage)
+            else:
+                raise DustbinContentError
+                
+        else:
+            if isinstance(garbage, Garbage):
+                self.house_waste_content.append(garbage)
+            else:
+                raise DustbinContentError
+            
+    def empty_contents(self):
+        self.plastic_content = []
+        self.paper_content = []
+        self.house_waste_content = []
+    
